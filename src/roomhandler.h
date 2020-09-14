4d3b05ed500142d64debd8a39e997d238232ffb3
tusikalanse <615822545@qq.com>
2020-09-14 11:06:04 +0800

    add class roomhandler

--- src/roomhandler.h
+++ src/roomhandler.h
@@ -4,6 +4,8 @@
 #include "mymemorypool.h"
 #include "room.h"
 
+#include <cstring>
+#include <string>
 #include <map>
 
 template<int MAX_ROOM_COUNT, int MAX_USER_COUNT_PER_ROOM>
@@ -12,7 +14,8 @@ class roomhandler {
   roomhandler();
   int init(int useripckey, int roomipckey);
   int registerUser(const char* username, const char* password);
-  int createRoom(int userid, const char* roomname);
+  int login(const char* username, const char* password);
+  int createRoom(const char* roomname);
   int joinRoom(int userid, int roomid);
   int exitRoom(int userid);
  private:
@@ -21,6 +24,7 @@ class roomhandler {
   int roomsize;
   std::map<int, room*> roomlist;
   std::map<int, user*> userlist;
+  std::map<std::string, user*> usernamelist;
   MyMemoryPool<sizeof(user)> userpool;
   MyMemoryPool<sizeof(room) + MAX_USER_COUNT_PER_ROOM * sizeof(user*)> roompool;
 };
@@ -33,11 +37,78 @@ roomhandler<MAX_ROOM_COUNT, MAX_USER_COUNT_PER_ROOM>::roomhandler() {
 
 template<int MAX_ROOM_COUNT, int MAX_USER_COUNT_PER_ROOM>
 int roomhandler<MAX_ROOM_COUNT, MAX_USER_COUNT_PER_ROOM>::init(int useripckey, int roomipckey) {
+  if (-1 == userpool.init(useripckey, MAX_USER_COUNT_PER_ROOM * MAX_ROOM_COUNT))
+    return -1;
+  if (-1 == roompool.init(roomipckey, MAX_ROOM_COUNT))
+    return -1;
   USER_IPCKEY = useripckey;
-  userpool.init(useripckey, MAX_USER_COUNT_PER_ROOM * MAX_ROOM_COUNT);
-  roompool.init(roomipckey, MAX_ROOM_COUNT);
+  ROOM_IPCKEY = roomipckey;
+  user::usercount = userpool.count();
+  for (int i = 0; i < MAX_USER_COUNT_PER_ROOM * MAX_ROOM_COUNT; ++i) {
+    if (userpool.getstatus(i)) {
+      user* tempUser = reinterpret_cast<user*>(userpool.get(i));
+      userlist[tempUser->userid] = tempUser;
+      usernamelist[std::string(tempUser->username)] = tempUser;
+    }
+  }
+  room::roomcount = roompool.count();
+  room::maxusercount = MAX_USER_COUNT_PER_ROOM;
+  for (int i = 0; i < MAX_ROOM_COUNT; ++i) {
+    if (roompool.getstatus(i)) {
+      room* tempRoom = reinterpret_cast<room*>(roompool.get(i));
+      roomlist[tempRoom->roomid] = tempRoom;
+    }
+  }
   roomsize = sizeof(room) + MAX_USER_COUNT_PER_ROOM * sizeof(user*);
+  return 0;
+} 
+
+template<int MAX_ROOM_COUNT, int MAX_USER_COUNT_PER_ROOM>
+int roomhandler<MAX_ROOM_COUNT, MAX_USER_COUNT_PER_ROOM>::registerUser(const char* username, const char* password) {
+  if (usernamelist.count(std::string(username))) return -1;
+  if (userpool.full()) return -2;
+  if (strlen(username) > 127 || strlen(password) > 127) return -3;
+  user* newUser = userpool.apply();
+  new (newUser) user(username, password);
+  userlist[newUser->userid] = newUser;
+  usernamelist[std::string(newUser->username)] = newUser;
+  return newUser->userid;
 }
 
+template<int MAX_ROOM_COUNT, int MAX_USER_COUNT_PER_ROOM>
+int roomhandler<MAX_ROOM_COUNT, MAX_USER_COUNT_PER_ROOM>::login(const char* username, const char* password) {
+  if (usernamelist.count(std::string(username)) == 0) return 0;
+  return usernamelist[std::string(username)]->login(password);
+}
+
+template<int MAX_ROOM_COUNT, int MAX_USER_COUNT_PER_ROOM>
+int roomhandler<MAX_ROOM_COUNT, MAX_USER_COUNT_PER_ROOM>::createRoom(const char* roomname) {
+  if (roompool.full()) return -1;
+  room* newRoom = roompool.apply();
+  new (newRoom) room(roomname);
+  roomlist[newRoom->roomid] = newRoom;
+  return newRoom->roomid;
+}
+
+template<int MAX_ROOM_COUNT, int MAX_USER_COUNT_PER_ROOM>
+int roomhandler<MAX_ROOM_COUNT, MAX_USER_COUNT_PER_ROOM>::joinRoom(int userid, int roomid) {
+  if (roomlist.count(roomid) == 0) return -1;
+  if (userlist.count(userid) == 0) return -2;
+  if (roomlist[roomid]->full()) return -3;
+  if (userlist[userid]->getroom() != 0) return -4;
+  if (userlist[userid]->joinroom(roomid) == -1) return -5;
+  roomlist[roomid]->adduser(userlist[userid]);
+  return 0;
+}
+
+template<int MAX_ROOM_COUNT, int MAX_USER_COUNT_PER_ROOM>
+int roomhandler<MAX_ROOM_COUNT, MAX_USER_COUNT_PER_ROOM>::exitRoom(int userid) {
+  if (userlist.count(userid) == 0) return -1;
+  int roomid = userlist[userid]->getroom();
+  if (roomid == 0) return -2;
+  roomlist[roomid]->removeuser(userid);
+  userlist[userid]->exitroom();
+  return 0;
+}
 
 #endif /* ROOM_HANDLER_H */
