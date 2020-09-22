ad49305f0af934be74fe10badbaa343157b75819
tusikalanse <615822545@qq.com>
2020-09-22 20:21:45 +0800

    add some functions

--- src/roomhandler.h
+++ src/roomhandler.h
@@ -7,6 +7,7 @@
 #include <cstring>
 #include <string>
 #include <map>
+#include <vector>
 
 template<int MAX_ROOM_COUNT, int MAX_USER_COUNT_PER_ROOM>
 class roomhandler {
@@ -21,11 +22,13 @@ class roomhandler {
   int createRoom(const char* roomname);
   int joinRoom(int userid, int roomid);
   int exitRoom(int userid);
+  const std::vector<room*>& getRoomList();
  private:
   int USER_IPCKEY;
   int ROOM_IPCKEY;
   int roomsize;
   std::map<int, room*> roomlist;
+  std::vector<room*> roomVector;
   std::map<int, user*> userlist;
   std::map<std::string, user*> usernamelist;
   MyMemoryPool<sizeof(user)> userpool;
@@ -46,7 +49,7 @@ int roomhandler<MAX_ROOM_COUNT, MAX_USER_COUNT_PER_ROOM>::init(int useripckey, i
     return -1;
   USER_IPCKEY = useripckey;
   ROOM_IPCKEY = roomipckey;
-  user::usercount = userpool.count();
+  user::usercount = userpool.size();
   for (int i = 0; i < MAX_USER_COUNT_PER_ROOM * MAX_ROOM_COUNT; ++i) {
     if (userpool.getstatus(i)) {
       user* tempUser = reinterpret_cast<user*>(userpool.get(i));
@@ -54,12 +57,13 @@ int roomhandler<MAX_ROOM_COUNT, MAX_USER_COUNT_PER_ROOM>::init(int useripckey, i
       usernamelist[std::string(tempUser->username)] = tempUser;
     }
   }
-  room::roomcount = roompool.count();
+  room::roomcount = roompool.size();
   room::maxusercount = MAX_USER_COUNT_PER_ROOM;
   for (int i = 0; i < MAX_ROOM_COUNT; ++i) {
     if (roompool.getstatus(i)) {
       room* tempRoom = reinterpret_cast<room*>(roompool.get(i));
       roomlist[tempRoom->roomid] = tempRoom;
+      roomVector.push_back(tempRoom);
     }
   }
   roomsize = sizeof(room) + MAX_USER_COUNT_PER_ROOM * sizeof(user*);
@@ -71,7 +75,7 @@ int roomhandler<MAX_ROOM_COUNT, MAX_USER_COUNT_PER_ROOM>::registerUser(const cha
   if (usernamelist.count(std::string(username))) return -1;
   if (userpool.full()) return -2;
   if (strlen(username) > 127 || strlen(password) > 127) return -3;
-  user* newUser = userpool.apply();
+  user* newUser = reinterpret_cast<user*>(userpool.apply());
   new (newUser) user(username, password);
   userlist[newUser->userid] = newUser;
   usernamelist[std::string(newUser->username)] = newUser;
@@ -87,7 +91,7 @@ int roomhandler<MAX_ROOM_COUNT, MAX_USER_COUNT_PER_ROOM>::login(const char* user
 template<int MAX_ROOM_COUNT, int MAX_USER_COUNT_PER_ROOM>
 int roomhandler<MAX_ROOM_COUNT, MAX_USER_COUNT_PER_ROOM>::getRoom(const char* username) {
   if (usernamelist.count(std::string(username)) == 0) return -1;
-  return usernamelist[std::string(newUser->username)]->getroom();
+  return usernamelist[std::string(username)]->getroom();
 }
 
 
@@ -100,16 +104,17 @@ const char* roomhandler<MAX_ROOM_COUNT, MAX_USER_COUNT_PER_ROOM>::getRoomName(in
 template<int MAX_ROOM_COUNT, int MAX_USER_COUNT_PER_ROOM>
 int roomhandler<MAX_ROOM_COUNT, MAX_USER_COUNT_PER_ROOM>::getUserID(const char* username) {
   if (usernamelist.count(std::string(username)) == 0) return -1;
-  return usernamelist[std::string(newUser->username)]->getuserid();
+  return usernamelist[std::string(username)]->getuserid();
 }
 
 
 template<int MAX_ROOM_COUNT, int MAX_USER_COUNT_PER_ROOM>
 int roomhandler<MAX_ROOM_COUNT, MAX_USER_COUNT_PER_ROOM>::createRoom(const char* roomname) {
   if (roompool.full()) return -1;
-  room* newRoom = roompool.apply();
+  room* newRoom = reinterpret_cast<room*>(roompool.apply());
   new (newRoom) room(roomname);
   roomlist[newRoom->roomid] = newRoom;
+  roomVector.push_back(newRoom);
   return newRoom->roomid;
 }
 
@@ -132,6 +137,11 @@ int roomhandler<MAX_ROOM_COUNT, MAX_USER_COUNT_PER_ROOM>::exitRoom(int userid) {
   roomlist[roomid]->removeuser(userid);
   userlist[userid]->exitroom();
   return 0;
+}
+
+template<int MAX_ROOM_COUNT, int MAX_USER_COUNT_PER_ROOM>
+const std::vector<room*>& roomhandler<MAX_ROOM_COUNT, MAX_USER_COUNT_PER_ROOM>::getRoomList() {
+  return roomVector;
 }
 
 #endif /* ROOM_HANDLER_H */
