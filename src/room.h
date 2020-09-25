103158fcbd433c918b1038143335d940768c4309
tusikalanse <615822545@qq.com>
2020-09-25 17:35:50 +0800

    add comment

--- src/room.h
+++ src/room.h
@@ -3,17 +3,42 @@
 
 #include "user.h"
 
+//房间结构体
+//保存房间id，名称，当前用户数量，每个用户指针
 struct room {
+  //用给定名称创建房间
   room(const char* _roomname);
+  
+  //按照id查找并移除用户
+  //成功移除返回0，否则返回-1(该用户不存在于当前房间)
   int removeuser(int userid);
+
+  //房间添加用户
+  //房间已满无法添加返回-1，成功添加返回0
   int adduser(user* newuser);
+
+  //返回房间是否为空
   int full();
+
+  //返回房间内用户数量
   int count();
+
+  //用于表示下一个房间的id
   static int roomcount;
+
+  //房间最大用户数量
   static int maxusercount;
+  
+  //房间id
   int roomid;
+
+  //房间名称
   char roomname[128];
+
+  //当前房间用户数量
   int usercount;
+
+  //用户指针，
   user* userlist[0];
 };
 
